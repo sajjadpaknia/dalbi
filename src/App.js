ac669a32ca88100d8443d42518c7060f0710291a
sajjad <sajjadpaknia369@gmail.com>
2022-12-20 14:33:03 +0330

    add some assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Home from "./Pages/Home/Home";
+// import Login from "./Pages/Login/Login";
+// import SignUp from "./Pages/SignUp/SignUp";
 
+axios.defaults.baseURL = "http://localhost:4433";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
