955e071a9cfd5c8ace56cb493b105b9ec993dfd4
sajjad <sajjadpaknia369@gmail.com>
2022-12-24 17:40:11 +0330

    Add Slider & Swiper

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,139 @@
+import classes from "./Slider.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination } from "swiper";
+import { Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <section className={classes.slider}>
+        <div className={classes.swiperItem}>
+          <Swiper
+            centeredSlides={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Autoplay, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/1.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/2.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/3.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/4.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/5.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/6.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/7.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/8.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/9.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link to={"#"}>
+                <img
+                  className={classes.image}
+                  src="./assets/images/slider/10.jpg"
+                  alt=""
+                />
+              </Link>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className={classes.item}>
+          <Link to={"#"}>
+            <img
+              className={classes.image}
+              src="./assets/images/adv (2).jpg"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className={classes.item}>
+          <Link to={"#"}>
+            <img
+              className={classes.image}
+              src="./assets/images/adv (1).jpg"
+              alt=""
+            />
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,13 @@
 import Container from "../../Components/Container/Container";
 import Header from "../../Components/Header/Header";
+import Slider from "../../Components/Slider/Slider";
 import classes from "./Home.module.css";
 export default function Home() {
   return (
     <>
       <Container>
         <Header />
-        <section className={classes.slider}>
-          <div>item1</div>
-          <div>item2</div>
-          <div>item3</div>
-        </section>
+        <Slider />
       </Container>
     </>
   );
